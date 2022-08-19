a801b3340af88dc081c85dd961f2a0296e062981
andreamunoz176 <72665263+andreamunoz176@users.noreply.github.com>
2022-08-18 22:07:03 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
-OLED.init(64, 128)
+OLED.init(128, 128)
 basic.forever(function () {
     if (tinkercademy.MoistureSensor(AnalogPin.P0) < 50) {
         OLED.writeStringNewLine("Moisture level is")
-        OLED.writeNumNewLine(tinkercademy.MoistureSensor(AnalogPin.P1))
+        OLED.writeNumNewLine(tinkercademy.MoistureSensor(AnalogPin.P0))
         OLED.writeStringNewLine("Water ur plant")
-        music.playTone(262, music.beat(BeatFraction.Whole))
+        tinkercademy.LED(DigitalPin.P1, OnOff.On)
+        basic.showIcon(IconNames.Asleep)
     } else {
         OLED.writeStringNewLine("Your plant is in good condition")
+        basic.showIcon(IconNames.Heart)
+        tinkercademy.LED(DigitalPin.P1, OnOff.Off)
     }
 })
